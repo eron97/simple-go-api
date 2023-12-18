b868a4c7a16ed73d157b304eae46f39b3a8e3efe
eron97 <eronsantosbetine@gmail.com>
2023-12-18 12:04:17 -0300

    alt endpoints

--- cmd/simple-go-api/main.go
+++ cmd/simple-go-api/main.go
@@ -32,8 +32,8 @@ func addName(c *gin.Context) {
 func main() {
 	r := gin.Default()
 
-	r.GET("/names", getAllNames)
-	r.POST("/names", addName)
+	r.GET("/listnames", getAllNames)
+	r.POST("/postnames", addName)
 
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
